35d84af76440d32ce28d5ea66663dcb938134b4d
Raja Babu <raja.b@terracetechlabs.com>
2019-04-15 12:40:19 +0530

    Added create wallet on user registration and fetch reports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,7 +26,7 @@ const RAKUTEN = {
  * @param role ("user", "admin", "manager", "support")
  */
 async function grantRole(userId: string, role: string) {
-	await admin.auth().setCustomUserClaims(userId, { role: role })
+	return admin.auth().setCustomUserClaims(userId, { role: role })
 }
 
 async function sendgridEmail(email, templateId, dynamic_template_data) {
@@ -42,8 +42,32 @@ async function sendgridEmail(email, templateId, dynamic_template_data) {
 			dynamic_template_data: dynamic_template_data,
 		}
 		const mailSent = await sgMail.send(message)
+		console.log(mailSent, 'A SENDGRID EMAIL SENT')
 	} catch (error) {
-		console.log('error: ', error)
+		console.error('error: SENDING MAIL ', error)
+	}
+}
+
+/**
+ * Create a wallet for user.
+ * Note: No one will be able to update wallet except admin.
+ */
+
+const createWallet = async uid => {
+	try {
+		return await admin
+			.firestore()
+			.collection('wallets')
+			.doc(uid)
+			.set({
+				userId: uid,
+				approvedAmount: 0,
+				pendingAmount: 0,
+				redeemedAmount: 0,
+			})
+	} catch (error) {
+		console.error('ERROR CREATING USER WALLET', error)
+		return error
 	}
 }
 
@@ -58,47 +82,66 @@ exports.onCreateUser = functions.auth.user().onCreate(async user => {
 		length: 10,
 		count: 1,
 	})[0]
-	if (!user.emailVerified) {
-		admin
-			.auth()
-			.generateEmailVerificationLink(user.email, actionCodeSettings)
-			.then(link => {
-				// Construct password reset email template, embed the link and send
-				// using custom SMTP server.
-				return sendgridEmail(
-					user.email,
 
-					'd-91f898ae86bc461d8b1b1e4806022e3f',
-					{
-						preheader: 'confirming emails makes us feel you are real',
-						header: 'Confirm your email',
-						body: 'Please confirm your email by tapping on below button.',
-						link: link,
-						buttonText: 'Confirm Email!',
-					}
-				)
+	try {
+		const userCollection = await admin
+			.firestore()
+			.collection('users')
+			.doc(user.uid)
+			.set({
+				uid: user.uid,
+				displayName: user.displayName,
+				firstName: name ? name[0] : null,
+				lastName: name
+					? name[name.length - 1] === undefined
+						? ''
+						: name[1]
+					: null,
+				email: user.email,
+				avatarURL: user.photoURL,
+				referralCode: referralCode,
+				referredBy: null, // Put here user id for refCode of the user.
+				paypalDetails: null,
+				role: 'user',
+				createdAt: admin.firestore.Timestamp.now(),
+				updatedAt: admin.firestore.Timestamp.now(),
 			})
-			.catch(error => {
-				// Some error occurred.
-			})
+		console.log('USER COLLECTION CREATED', userCollection)
+		await grantRole(user.uid, 'user')
+		console.log('USER ROLE GRANTEDT')
+		await createWallet(user.uid)
+		console.log('WALLET CREATED', user.uid)
+
+		// Above steps are neccessary and it must not fail.
+
+		if (!user.emailVerified) {
+			admin
+				.auth()
+				.generateEmailVerificationLink(user.email, actionCodeSettings)
+				.then(link => {
+					// Construct password reset email template, embed the link and send
+					// using custom SMTP server.
+					return sendgridEmail(
+						user.email,
+
+						'd-91f898ae86bc461d8b1b1e4806022e3f',
+						{
+							preheader: 'confirming emails makes us feel you are real',
+							header: 'Confirm your email',
+							body: 'Please confirm your email by tapping on below button.',
+							link: link,
+							buttonText: 'Confirm Email!',
+						}
+					)
+				})
+				.catch(error => {
+					// Some error occurred.
+					console.log('ERROR SENDING VERIFICATION EMAIL', error)
+				})
+		}
+	} catch (error) {
+		console.error('ERROR IN ON USER CREATION', error)
 	}
-	return admin
-		.firestore()
-		.collection('users')
-		.doc(user.uid)
-		.set({
-			uid: user.uid,
-			firstName: name ? name[0] : null,
-			lastName: name ? (name[name.length] === undefined ? '' : name[1]) : null,
-			email: user.email,
-			avatarURL: user.photoURL,
-			referralCode: referralCode,
-			referredBy: null, // Put here user id for refCode of the user.
-			paypalDetails: null,
-			createdAt: admin.firestore.Timestamp.now(),
-			updatedAt: admin.firestore.Timestamp.now(),
-		})
-		.then(() => grantRole(user.uid, 'user'))
 })
 
 /**
@@ -276,4 +319,87 @@ exports.fetchStores = functions.https.onCall(async (data, context) => {
 	// }
 
 	return getRakutenStores()
+})
+
+const getReports = async (start_date, end_date) => {
+	const axios = require('axios')
+	const Papa = require('papaparse')
+
+	try {
+		const URL = `https://ran-reporting.rakutenmarketing.com/en/reports/newly/filters?start_date=${start_date}&end_date=${end_date}&include_summary=N&network=1&tz=GMT&date_type=transaction&token=ZW5jcnlwdGVkYToyOntzOjU6IlRva2VuIjtzOjY0OiI0ODhhNWNiZWRiNjIxNzhkZTU0NTQ5ZTY5NWQzYTZkYjA5NDJjMjZkYWIxYTNlNmNkOTM5NTFkYzY1ZDAwOGM2IjtzOjg6IlVzZXJUeXBlIjtzOjk6IlB1Ymxpc2hlciI7fQ%3D%3D`
+
+		const response = await axios.get(URL)
+		const result = Papa.parse(response.data, {
+			header: true,
+			dynamicTyping: true,
+		})
+		console.log(result.data[0])
+		result.data.pop()
+		return result.data
+	} catch (error) {
+		console.log('error in getting affiliate partner details: ', error)
+		throw new functions.https.HttpsError('cancelled', 'Something went wrong!')
+	}
+}
+
+exports.fetchReports = functions.https.onCall(async (data, context) => {
+	// Checking that the user is authenticated.
+	if (!context.auth) {
+		// Throwing an HttpsError so that the client gets the error details.
+		throw new functions.https.HttpsError(
+			'failed-precondition',
+			'The function must be called ' + 'while authenticated.'
+		)
+	}
+
+	//TODO: implement role management and then uncomment below.
+
+	// // Checking that user is admin or manager
+	// const adminRecord = await admin.auth().getUser(context.auth.uid)
+
+	// const isAdmin = (adminRecord.customClaims as any).role === 'admin'
+	// const isManager = (adminRecord.customClaims as any).role === 'manager'
+	// if (!isAdmin || !isManager) {
+	// 	// Throwing an HttpsError so that the client gets the error details.
+	// 	throw new functions.https.HttpsError(
+	// 		'failed-precondition',
+	// 		'The function must be called ' + 'only by an administrator'
+	// 	)
+	// }
+
+	// try {
+	// 	const getAffiliateDetails = await admin
+	// 		.firestore()
+	// 		.collection('affiliatePartners')
+	// 		.doc(data.affid)
+	// 		.get()
+	// } catch (error) {
+	// 	console.log('error: ', error)
+	// 	throw new functions.https.HttpsError(
+	// 		'failed-precondition',
+	// 		'The function must be called ' + 'only by an administrator'
+	// 	)
+	// }
+
+	const reportData = await getReports('2017-04-01', '2019-04-01')
+
+	const reportsBatch = admin.firestore().batch()
+
+	if (reportData.length > 0) {
+		for (let i = 0; i < reportData.length; i++) {
+			const transRef = admin
+				.firestore()
+				.collection('transactions')
+				.doc()
+			reportsBatch.set(transRef, {
+				clickId: reportData[i]['Member ID (U1)'],
+				userId: 'kbl4PmeKjDMoFBQv6uxK5birWdr2',
+				affiliateData: { ...reportData[i] },
+				storeId: reportData[i]['Advertiser Name'],
+				totalCommission: reportData[i]['Total Commission'],
+				lineItems: null,
+			})
+		}
+	}
+	return reportsBatch.commit()
 })
