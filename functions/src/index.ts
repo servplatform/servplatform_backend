ae940723aade0ea1c3267298a0768b119bce7cc1
Raja Babu <rbtheroot@gmail.com>
2019-10-25 17:40:22 +0530

    stripe and agrregate

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as _ from 'lodash'
+import { print } from 'util'
 
 admin.initializeApp()
 
+const stripe = require('stripe')(functions.config().stripe.token)
 // Constants
 
 const roles = ['user', 'admin', 'manager', 'support']
@@ -197,3 +199,218 @@ exports.setRole = functions.https.onCall(async (data, context) => {
 	// Set claims on firebase user and firestore user
 	return grantRole(userId, role)
 })
+
+/**
+ * STRIPE CHARGES
+ */
+
+/**
+ * Only admin is allowed to execute this function.
+ */
+
+exports.createStripeChareg = functions.https.onCall(async (data, context) => {
+	// Checking that the user is authenticated.
+	if (!context.auth) {
+		// Throwing an HttpsError so that the client gets the error details.
+		throw new functions.https.HttpsError(
+			'failed-precondition',
+			'The function must be called ' + 'while authenticated.'
+		)
+	}
+
+	const userId = context.auth.uid
+
+	const token = data.token
+	const cart = data.cart
+	const totalAmount = data.totalAmount
+	const taxAmount = data.taxAmount
+	const subTotal = data.subTotal
+
+	console.log(data)
+
+	// TODO: type checking and calculate amount using cart.
+
+	try {
+		const response = await stripe.paymentIntents.create({
+			amount: parseInt((totalAmount * 100).toString(), 10),
+			currency: 'usd',
+		})
+
+		const orderRef = admin
+			.firestore()
+			.collection('orders')
+			.doc(response.id)
+
+		await orderRef.set({
+			cart: cart,
+			payment: response,
+			totalAmount: totalAmount,
+			taxAmount: taxAmount,
+			subTotal: subTotal,
+			paymentId: response.id,
+			status: 'created',
+			status1: 'received',
+			userId: userId,
+			createdAt: admin.firestore.Timestamp.now(),
+			updatedAt: admin.firestore.Timestamp.now(),
+		})
+		return { ...response, orderId: orderRef.id }
+	} catch (error) {
+		console.log(error)
+		throw new functions.https.HttpsError(
+			'aborted',
+			'Something went wrong. Try again latter'
+		)
+	}
+})
+
+/// Stripe webhook for payment intent
+
+exports.stripeWebhook = functions.https.onRequest(async (request, response) => {
+	const endpointSecret = 'whsec_iEpuKW86nY1IGcKyUB8zwjkka1YO5IRR'
+	const sig = request.headers['stripe-signature']
+	const body = request.rawBody
+
+	console.log(request.headers)
+
+	let event = null
+
+	try {
+		event = stripe.webhooks.constructEvent(request.rawBody, sig, endpointSecret)
+		console.log(event)
+	} catch (err) {
+		console.log(err)
+		response.status(400).end()
+		return null
+	}
+
+	let intent = null
+	switch (event['type']) {
+		case 'payment_intent.succeeded':
+			intent = event.data.object
+			console.log('Succeeded:', intent.id)
+			try {
+				await admin
+					.firestore()
+					.collection('orders')
+					.doc(intent.id)
+					.update({
+						status: 'paid',
+						updatedAt: admin.firestore.Timestamp.now(),
+					})
+			} catch (error) {
+				console.log('ERROR UPDATING ORDER: ', error)
+				response.sendStatus(200)
+			}
+			break
+		case 'payment_intent.payment_failed':
+			intent = event.data.object
+			const message =
+				intent.last_payment_error && intent.last_payment_error.message
+			try {
+				await admin
+					.firestore()
+					.collection('orders')
+					.doc(intent.id)
+					.update({
+						status: 'failed',
+						message: message,
+						updatedAt: admin.firestore.Timestamp.now(),
+					})
+				return response.sendStatus(200)
+			} catch (error) {
+				console.log('ERROR UPDATING ORDER: ', error)
+				return response.sendStatus(200)
+			}
+			break
+	}
+
+	return response.sendStatus(200)
+})
+
+// DELETE CART ITEM
+
+exports.onOrderCreate = functions.firestore
+	.document('orders/{orderId}')
+	.onCreate(async (snap, context) => {
+		const orderData = snap.data()
+		const cartData = orderData.cart
+
+		try {
+			for (const item of cartData) {
+				await admin
+					.firestore()
+					.collection('carts')
+					.doc(item.cartId)
+					.update({
+						deleted: true,
+						ordered: true,
+					})
+					.then(() => console.log('cart updated', item.cartId))
+
+				await admin
+					.firestore()
+					.collection('products')
+					.doc(item.productId)
+					.update({
+						inventoryQty: admin.firestore.FieldValue.increment(-item.qty),
+					})
+					.then(() => console.log('product updated', item.productId))
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	})
+
+exports.onOrderUpdate = functions.firestore
+	.document('orders/{orderId}')
+	.onUpdate(async (change, context) => {
+		const prevData = change.before.data()
+		const newData = change.after.data()
+
+		if (newData === null) {
+			return
+		}
+		if (newData.status !== 'paid' || newData.status !== 'failed') {
+			return
+		}
+		if (newData.status === 'paid') {
+			const cartData = newData.cart
+
+			try {
+				for (const item of cartData) {
+					await admin
+						.firestore()
+						.collection('products')
+						.doc(item.productId)
+						.update({
+							inventoryQty: admin.firestore.FieldValue.increment(-item.qty),
+						})
+						.then(() => console.log('product updated', item.productId))
+				}
+			} catch (error) {
+				console.log(error)
+				return
+			}
+		} else if (newData.status === 'failed') {
+			const cartData = newData.cart
+
+			try {
+				for (const item of cartData) {
+					await admin
+						.firestore()
+						.collection('products')
+						.doc(item.productId)
+						.update({
+							inventoryQty: admin.firestore.FieldValue.increment(item.qty),
+						})
+						.then(() => console.log('product updated', item.productId))
+				}
+			} catch (error) {
+				console.log(error)
+				return
+			}
+		}
+	})
+
+///
